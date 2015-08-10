37cff7649d498cfb845af1a698b986f1efb42793
Samuel Spencer <theodore.therone@gmail.com>
2015-08-10 01:38:01 +0000

    remove cruft

--- django_spaghetti/views.py
+++ django_spaghetti/views.py
@@ -21,22 +21,39 @@ def plate(request):
         _id = "%s__%s"%(model.app_label,model.model)
         label = "%s"%(model.model)
         fields = [f for f in model.model_class()._meta.fields if not str(f.name).endswith('_ptr')]
+        many = [f for f in model.model_class()._meta.many_to_many]
         if graph_settings.get('show_fields',True):
             label += "\n%s\n"%("-"*len(model.model))
             label += "\n".join([str(f.name) for f in fields])
+        edge_color = {'inherit':'from'}
+
         for f in fields:
             f.ftype = str(f.__class__).split('.')[-1][:-2]
             if type(f) == related.ForeignKey:
-                #print dir(f)
                 _to = tuple(str(f.related_field).lower().split('.')[0:2])
-                #print f, f.related_field
+                if _to[0] != model.app_label:
+                    edge_color = {'inherit':'both'}
                 edges.append(
                     {
                         'from':_id,
                         'to':"%s__%s"%_to,
-                        'arrows':{'to':{'scaleFactor':0.5}}
+                        'color':edge_color,
+                        'arrows':{'to':{'scaleFactor':0.75}}
                     }
                 )
+        for f in many:
+            #print dir(f)
+            m = f.rel.to._meta
+            #if m.app_label != model.app_label:
+            edge_color = {'color':'gray'}
+            edges.append(
+                {
+                    'from':_id,
+                    'to':"%s__%s"%(m.app_label,m.model_name),
+                    'color':edge_color,
+                    'arrows':{'to':{'scaleFactor':0.5}, 'from':{'scaleFactor':0.5}},
+                }
+            )
         nodes.append(
             {
                 'id':_id,
