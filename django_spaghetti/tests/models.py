bd068d2b7418933a2739d7dea9ae1d0523a61ee7
Samuel Spencer <theodore.therone@gmail.com>
2015-12-14 23:24:10 +0000

    Bumping the version number as this now drops Django 1.7 support

--- django_spaghetti/tests/models.py
+++ django_spaghetti/tests/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 class PoliceOfficer(models.Model):
     """
-    An officer of the NYPD
+    An officer of the NYPD.
+    E.g. "Jake Peralta"
     """
     badge_number = models.IntegerField(primary_key=True)
     first_name = models.CharField(max_length=200)
@@ -16,6 +17,10 @@ class PoliceStation(models.Model):
     pass
 
 class Precinct(PoliceStation):
+    """
+    A precinct of officers
+    E.g. "Brookyln 99"
+    """
     number = models.IntegerField(primary_key=True)
     burrough = models.CharField(max_length=20)
     captain = models.OneToOneField(PoliceOfficer)
@@ -25,6 +30,10 @@ class Precinct(PoliceStation):
         return (self.burrough,self.number)
 
 class Division(PoliceStation):
+    """
+    A division of officers, not in the field.
+    E.g. Major Crimes Unit
+    """
     name = models.CharField(max_length=200)
     
 class Arrest(models.Model):
@@ -32,5 +41,10 @@ class Arrest(models.Model):
     perp = models.ForeignKey("Perpetrator")
 
 class Perpetrator(models.Model):
+    """
+    A person who is accused of a crime.
+    E.g. Doug Judy, aka. "The Pontiac Bandit"
+    """
     first_name = models.CharField(max_length=200)
     surname = models.CharField(max_length=200)
+    alias = models.CharField(max_length=200)
