b98839de990e3c4b1cd1a5730df512c2f9bc472a
tobybellwood <theodore.therone@gmail.com>
2016-02-24 04:54:33 +0000

    add test models

--- django_spaghetti/tests/models.py
+++ django_spaghetti/tests/models.py
@@ -10,15 +10,13 @@ class PoliceOfficer(models.Model):
     rank = models.CharField(max_length=200)
     
     arrests = models.ManyToManyField("Arrest",related_name="arresting_officers")
-    station = models.ForeignKey("PoliceStation",related_name="officers")
-
-class PoliceStation(models.Model):
-    pass
 
 class Precinct(PoliceStation):
-    number = models.IntegerField(max_length=10, primary_key=True)
+    number = models.IntegerField(max_length=10)
     burrough = models.CharField(max_length=20)
     captain = models.OneToOneField(PoliceOfficer)
+    officers = models.ForeignKey("PoliceOfficer",related_name="precinct")
+    
     class Meta:
         unique_together = ("burrough","number")
     def natural_key(self):
@@ -26,11 +24,15 @@ class Precinct(PoliceStation):
 
 class Division(PoliceStation):
     name = models.CharField(max_length=200)
-    
+    officers = models.ForeignKey("PoliceOfficer",related_name="division")
+
 class Arrest(models.Model):
     alleged_crime = models.CharField(max_length=20)
     perp = models.ForeignKey("Perpetrator")
+    arrest_date = models.DateField()
+    processing_date = models.DateField()
 
 class Perpetrator(models.Model):
     first_name = models.CharField(max_length=200)
     surname = models.CharField(max_length=200)
+    birth_date = models.DateField()
