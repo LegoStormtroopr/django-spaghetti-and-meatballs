64e100983fc8618e5b84633c01e0392bc3455e98
Samuel Spencer <theodore.therone@gmail.com>
2016-07-06 05:00:46 +0000

    py3

--- django_spaghetti/tests/test_it.py
+++ django_spaghetti/tests/test_it.py
@@ -9,15 +9,15 @@ class LoadThePlate(TestCase):
     def test_plate(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
-        self.assertTrue('Officer' in response.content)
+        self.assertTrue('Officer' in str(response.content))
 
     def test_plate_with_settings(self):
         response = self.client.get("/test/plate_settings")
         self.assertEqual(response.status_code, 200)
-        self.assertTrue('Officer' not in response.content)
+        self.assertTrue('Officer' not in str(response.content))
 
     def test_plate_with_override_settings(self):
         response = self.client.get("/test/plate_override")
         self.assertEqual(response.status_code, 200)
-        self.assertTrue('policeofficer' in response.content.lower())
-        self.assertTrue('policestation' not in response.content.lower())
+        self.assertTrue('policeofficer' in str(response.content).lower())
+        self.assertTrue('policestation' not in str(response.content).lower())
