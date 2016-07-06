2b2052298b5fede78c4bebb5b6a12bcfab39470c
Samuel Spencer <theodore.therone@gmail.com>
2016-07-06 04:53:32 +0000

    more files

--- django_spaghetti/tests/urls.py
+++ django_spaghetti/tests/urls.py
@@ -5,19 +5,17 @@ from django_spaghetti.views import plate, Plate
 urlpatterns = [
     url(r'^$', plate, name='plate'),
     url(r'^test/plate_settings$', Plate.as_view(
-        settings = {
+        settings={
             'apps': ['auth'],
             'exclude': {},
         }
-        
     ), name='test_plate_settings'),
     url(r'^test/plate_override$', Plate.as_view(
-        override_settings = {
+        override_settings={
             'exclude': {
-                'tests':['policestation']
+                'tests': ['policestation']
             },
         },
-        meatball_template_name = "tests/meatball.html"
-        
+        meatball_template_name="tests/meatball.html"
     ), name='test_plate_override$'),
 ]
