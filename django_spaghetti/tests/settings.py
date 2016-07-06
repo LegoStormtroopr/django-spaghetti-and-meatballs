bedc09f21db29a6cd0fe42d5a585c410aa4757a5
Samuel Spencer <theodore.therone@gmail.com>
2016-07-06 04:45:39 +0000

    pep

--- django_spaghetti/tests/settings.py
+++ django_spaghetti/tests/settings.py
@@ -26,7 +26,7 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
-TEMPLATE_DIRS = [os.path.join(BASE_DIR,'templates')]
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 # Application definition
 
 INSTALLED_APPS = (
